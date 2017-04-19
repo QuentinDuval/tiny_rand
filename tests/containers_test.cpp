da27fe0d2d0a855b78bf226af18b67b9caa160fc
Duval Quentin <senmenty@gmail.com>
2017-04-19 17:10:03 +0200

    refactor string test

--- tests/containers_test.cpp
+++ tests/containers_test.cpp
@@ -8,21 +8,32 @@
 #include "tiny_rand/generators.h"
 #include "test_utils.h"
 
+using namespace tiny_rand;
 
 struct ContainersTest : public ::testing::Test
 {
    ContainersTest() : m_bit_gen(0) {}
    std::mt19937 m_bit_gen;
+
+   template <typename ContainerGen>
+   void sequential_container_test(ContainerGen container_gen)
+   {
+      std::vector<char> char_set = { 'a', 'b', 'c' };
+      auto gen = container_gen(2, 10, choice_gen(char_set));
+
+      auto val = gen(m_bit_gen);
+      EXPECT_GE(val.size(), 2);
+      EXPECT_LE(val.size(), 10);
+      ASSERT_TRUE(all_of(val, [&](auto c) {
+         return contains(char_set, c);
+      }));
+   }
 };
 
 TEST_F(ContainersTest, generation_of_strings)
 {
-   std::vector<char> char_set = { 'a', 'b', 'c' };
-   auto gen = tiny_rand::string_gen(10, tiny_rand::choice_gen(char_set));
-
-   std::string val = gen(m_bit_gen);
-   EXPECT_LE(val.size(), 10);
-   ASSERT_TRUE(all_of(val, [&](auto c) {
-      return contains(char_set, c);
-   }));
+   sequential_container_test(
+      [](int min_size, int max_size, auto gen) {
+         return string_gen(int_gen(min_size, max_size), gen);
+      });
 }
